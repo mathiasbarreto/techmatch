ea31e6e32b284352734f4800924debde5342522f
mathias barreto <mathiasluz.pro@gmail.com>
2021-05-31 17:49:56 -0300

    mvc create job reviews + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :jobs, only: :index, :new, :create
-  resources :offers
-  resources :users
+  resources :offers do
+    resources :jobs, only: :index, :new, :create 
+    end
+  end  
+  
+  resources :users do 
+  end
 end
