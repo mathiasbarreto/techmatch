e66930d6d2f401748828862639a51cf4d4f899a9
Perrine Lardilleux <perrinelardilleux@gmail.com>
2021-05-31 17:12:30 -0300

    banner e footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_185414) do
+ActiveRecord::Schema.define(version: 2021_05_31_191717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "offers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_offers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_05_31_185414) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "offers", "users"
 end
