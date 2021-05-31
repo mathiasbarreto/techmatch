95836465a34deacf7fc5d2862d2479f25c450810
Lucas Pessoa <lcppcoding@gmail.com>
2021-05-31 16:18:28 -0300

    added offer model, again

---
+++ db/migrate/20210531191717_create_offers.rb
@@ -0,0 +1,10 @@
+class CreateOffers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :offers do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
