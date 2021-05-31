172542c25c8829eeacff74e00923e1ff8e03b187
Thiago Youssef <thiagodyoussef@gmail.com>
2021-05-31 19:54:37 -0300

    Sign Up and Login style

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :users do
-  #   resources :offers, only: [:new, :create]
-  #   resources :jobs, only: [:new, :create]
-  # end
-  # resources :offers, except: [:new, :create]
-  # resources :jobs, only: [:index]
+  resources :users do
+    resources :offers, only: [:new, :create]
+    resources :jobs, only: [:new, :create]
+  end
+  resources :offers, except: [:new, :create]
+  resources :jobs, only: [:index]
 end
