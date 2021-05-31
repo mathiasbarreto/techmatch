864a9ec31ba5882b0a3c68d50338db0189267c5a
mathias barreto <mathiasluz.pro@gmail.com>
2021-05-31 17:47:49 -0300

    mvc create job reviews + routes

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,6 +4,20 @@ class JobsController < ApplicationController
   end
 
   def show
-    @job = Job.new 
+    @job = Job.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review = Job.find(params[:job_id])
+    @review.job = @review
+    @review.save
+    redirect_to jobs_path(@job)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:contractor_rating, :contractor_review, :employer_review, :employer_rating, :start_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "jobs", to: "jobs#index"
-  get "/jobs/:id/new", to: "jobs#new"
-  post "jobs", to: "jobs#create"
+  resources :jobs, only: :index, :new, :create
+  resources :offers
+  resources :users
 end
