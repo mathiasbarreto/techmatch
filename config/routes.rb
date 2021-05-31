a80712ea298472788362d7de50d717767c3fbf37
mathias barreto <mathiasluz.pro@gmail.com>
2021-05-31 16:47:44 -0300

    mvc jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "jobs", to: "jobs#index"
+  get "jobs/new", to: "jobs#new"
 end
