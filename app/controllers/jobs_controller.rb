f2f9f23251ed1d7513a35bdbe0abe787282c9507
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-01 02:48:19 -0300

    hacked jobs#create into place, still needs work

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,23 +1,33 @@
+require 'date'
+
 class JobsController < ApplicationController
   def index
     @jobs = Job.all
   end
 
   def show
-    @job = Job.new
+    # @job = Job.new
+    @job = Job.find(params[:id])
   end
 
   def create
-    @review = Review.new(review_params)
-    @review = Job.find(params[:job_id])
-    @review.job = @review
-    @review.save
-    redirect_to jobs_path(@job)
+    # @review = Review.new(review_params)
+    # @review = Job.find(params[:job_id])
+    # @review.job = @review
+    # @review.save
+    # redirect_to jobs_path(@job)
+    @job = Job.new(user_id: params[:user_id], offer_id: params[:offer_id])
+    @job.start_date = Date.today
+    if @job.save
+      redirect_to jobs_path
+    else
+      redirect_to offer_path(offer_id)
+    end
   end
 
   private
 
-  def review_params
-    params.require(:review).permit(:contractor_rating, :contractor_review, :employer_review, :employer_rating, :start_date)
+  def job_params
+    params.require(:review).permit(:contractor_rating, :contractor_review, :employer_review, :employer_rating, :start_date, :user_id, :employer_id)
   end
 end
