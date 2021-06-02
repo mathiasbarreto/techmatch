8df5344088f12baa9a16980702f2f4fadee10016
mathias barreto <mathiasluz.pro@gmail.com>
2021-06-02 15:24:38 -0300

    old stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_191602) do
+ActiveRecord::Schema.define(version: 2021_06_02_135922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2021_06_01_191602) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
     t.text "description"
-    t.string "status", default: "unfulfilled"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
