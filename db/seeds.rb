58d3d4ee5f1eae8249b78e84fd4a77990123b7e8
Lucas Pessoa <lcppcoding@gmail.com>
2021-06-03 17:37:22 -0300

    changes to seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@ User.destroy_all
 skills = %w[ruby rails javascript html css sql python java c c++ c# spring django react statistics]
 prices = [15, 50, 70, 100, 150, 250, 500, 1000, 1500, 2300, 5000, 10000] # Somehow I can't think of a better single line solution
 
-puts 'Creating 30'
-30.times do |i|
+puts 'Creating 50 users'
+50.times do |i|
   user = User.create!(
     email: Faker::Internet.unique.email,
     username: Faker::Name.unique.first_name,
@@ -22,8 +22,8 @@ puts 'Creating 30'
   user.photo.attach(io: file, filename: Faker::Lorem.unique.word)
 end
 
-puts "Creating 50 offers"
-50.times do |i|
+puts "Creating 200 offers"
+200.times do |i|
   offer = Offer.create!(
     title: [Faker::Hacker.verb, Faker::Hacker.adjective, Faker::Hacker.noun, 'and', Faker::Hacker.noun].join(" ").capitalize,
     description: Faker::Lorem.paragraphs(number: rand(2..4))[0].capitalize,
@@ -32,16 +32,16 @@ puts "Creating 50 offers"
   )
 end
 
-puts "Creating 20 jobs"
-20.times do |i|
+puts "Creating 100 jobs"
+100.times do |i|
   user = User.all.sample
   job = Job.create!(
     user: user,
     offer: Offer.where("user_id != ?", user.id).sample,
     start_date: Date.today - rand(16..60),
-    contractor_review: Faker::Lorem.paragraphs(number: rand(2..4)),
+    contractor_review: Faker::Lorem.paragraphs(number: rand(2..4))[0],
     contractor_rating: rand(3.0..10.0).floor(1),
-    employer_review: Faker::Lorem.paragraphs(number: rand(2..4)),
+    employer_review: Faker::Lorem.paragraphs(number: rand(2..4))[0],
     employer_rating: rand(3.0..10.0).floor(1),
     finish_date: Date.today - rand(0..15)
   	)
